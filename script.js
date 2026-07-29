b1e43477afa48e8f80e75599d0cd02a1270f579c
tozummi <tozummi@gmail.com>
2026-07-29 13:42:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ window.addEventListener("message", function (event) {
     }
   }
 });
+
+window.addEventListener("message", function (event) {
+  if (event.origin !== "https://tozummi.github.io") {
+    return;
+  }
+
+  if (
+    event.data &&
+    event.data.type === "prayer-widget-height"
+  ) {
+    const iframe = document.getElementById(
+      "prayer-times-widget"
+    );
+
+    if (iframe) {
+      iframe.style.height = `${event.data.height + 20}px`;
+    }
+  }
+});
