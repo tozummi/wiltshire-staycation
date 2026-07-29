edf7e4617ec097dc68cc8b0216eb93a2b139d09e
tozummi <tozummi@gmail.com>
2026-07-29 13:22:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // Wiltshire Staycation Website
 // JavaScript will be added here as we build the site.
+
+window.addEventListener("message", function (event) {
+  if (event.origin !== "https://tozummi.github.io") {
+    return;
+  }
+
+  if (
+    event.data &&
+    event.data.type === "photo-widget-height"
+  ) {
+    const iframe = document.getElementById(
+      "photo-of-the-day-widget"
+    );
+
+    if (iframe) {
+      iframe.style.height = `${event.data.height + 20}px`;
+    }
+  }
+});
