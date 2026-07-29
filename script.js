7c67cb1d08eabc091ffadd0c2522d6b1f91a2437
tozummi <tozummi@gmail.com>
2026-07-29 23:01:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -536,3 +536,32 @@ async function loadStayForecast() {
 
 updateStayDayHeading();
 loadStayForecast();
+
+const scrollToTopButton =
+  document.getElementById("scroll-to-top");
+
+if (scrollToTopButton) {
+  function updateScrollToTopButton() {
+    const shouldShow = window.scrollY > 500;
+
+    scrollToTopButton.classList.toggle(
+      "is-visible",
+      shouldShow
+    );
+  }
+
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  window.addEventListener(
+    "scroll",
+    updateScrollToTopButton,
+    { passive: true }
+  );
+
+  updateScrollToTopButton();
+}
